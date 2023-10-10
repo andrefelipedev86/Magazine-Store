ac3fad5f2df3c9e4013c24d5c76186bae9069432
André Felipe <andrefps007@hotmail.com>
2023-10-10 18:47:29 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+const catalog = [
+  {
+    id: 1,
+    brand: 'Louis Vuitton',
+    name: 'Camisa Larga com Bolsos',
+    price: 70,
+    image: 'product-1.jpg',
+    female: false
+  },
+  {
+    id: 2,
+    brand: 'Gucci',
+    name: 'Casaco Reto com Lã',
+    price: 85,
+    image: 'product-2.jpg',
+    female: true
+  },
+  {
+    id: 3,
+    brand: 'Louis Vuitton',
+    name: 'Jaqueta com Efeito Camurça',
+    price: 60,
+    image: 'product-3.jpg',
+    female: false
+  },
+  {
+    id: 4,
+    brand: 'Guess',
+    name: 'Sobretudo em Mescla de Lã',
+    price: 160,
+    image: 'product-4.jpg',
+    female: false
+  },
+  {
+    id: 5,
+    brand: 'Hollister',
+    name: 'Camisa Larga Acolchoada de Veludo Cotelê',
+    price: 110,
+    image: 'product-5.jpg',
+    female: false
+  },
+  {
+    id: 6,
+    brand: 'Lacoste',
+    name: 'Casaco de Lã com Botões',
+    price: 170,
+    image: 'product-6.jpg',
+    female: true
+  },
+  {
+    id: 7,
+    brand: 'Calvin Klein',
+    name: 'Casaco com Botões',
+    price: 75,
+    image: 'product-7.jpg',
+    female: true
+  },
+  {
+    id: 8,
+    brand: 'Tommy Hilfinger',
+    name: 'Colete Comprido com Cinto',
+    price: 88,
+    image: 'product-8.jpg',
+    female: true
+  }
+]
+
+
+for( const productCatalog of catalog) {
+  const productCard = `<div id="product-card-1">
+  <img 
+  src="./assets/img/${productCatalog.image}" alt="">
+  <p>${productCatalog.brand}</p>
+  <p>${productCatalog.name}</p>
+  <p>$${productCatalog.price}</p>
+  <button>Adicionar</button>
+  </div>`
+  
+  document.getElementById('product-container').innerHTML += productCard
+}
+
+
+
